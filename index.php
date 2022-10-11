f4062f8e7396876a5f0dfe781c71f99a80ddb239
samu <samueljose.garcia@iesdonana.org>
2022-10-11 21:16:47 +0200

    clase 1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Departamentos</title>
+</head>
+<body>
+    <?php
+
+    $pdo = new PDO('pgsql:host=localhost;dbname=empresa', 'empresa', 'empresa');
+
+    $sent = $pdo->query('SELECT * FROM departamentos');
+
+    $filas = $sent->fetchAll();
+
+    print_r($filas);
+
+
+
+    ?>
+</body>
+</html>
