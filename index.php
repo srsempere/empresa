0886ea6439d599d0aeb3b731ace2a1ffef5427d4
samu <samueljose.garcia@iesdonana.org>
2022-10-13 20:17:17 +0200

    Antes de indicar un rango de códigos a buscar

--- index.php
+++ index.php
@@ -1,30 +1,45 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Departamentos</title>
 </head>
+
 <body>
     <?php
-
+    $codigo = (isset($_GET['codigo'])) ? trim($_GET['codigo']) : null;
+    ?>
+    <div>
+        <form action="" method="get">
+            <label>
+                Código:
+                <input type="text" name="codigo" size="8" value="<?= $codigo ?>">
+                <button type="submit">buscar</button>
+            </label>
+        </form>
+    </div>
+    <?php
     $pdo = new PDO('pgsql:host=localhost;dbname=empresa', 'empresa', 'empresa');
-
-    $sent = $pdo->query('SELECT COUNT(*) FROM departamentos');
+    $pdo->beginTransaction();
+    $sent = $pdo->query('LOCK TABLE departamentos IN SHARE MODE');
+    $sent = $pdo->prepare('SELECT COUNT(*) FROM departamentos WHERE codigo = :codigo');
+    $sent->execute([':codigo => $codigo']);
     $total = $sent->fetchColumn();
     $sent = $pdo->query('SELECT * FROM departamentos ORDER BY codigo');
-
+    $pdo->commit();
     ?>
 
-    <div></div>
+    <div>
         <table style="margin: auto" border="1">
             <thead>
                 <th>Código</th>
                 <th>Denominación</th>
             </thead>
             <tbody>
-                <?php foreach ($sent as $fila): ?>
+                <?php foreach ($sent as $fila) : ?>
                     <tr>
                         <td><?= $fila['codigo'] ?></td>
                         <td><?= $fila['denominacion'] ?></td>
@@ -32,7 +47,8 @@
                 <?php endforeach ?>
             </tbody>
         </table>
-
-        <p>Número total de filas:<?= $total ?> </p>
+    </div>
+    <p>Número total de filas:<?= $total ?> </p>
 </body>
+
 </html>
